468344571580e40fd5fc85ad91e7e35520b2f9da
Joshua Ramirez <josh@Joshuas-MacBook-Pro.local>
2017-03-17 09:41:50 -0400

    Fix mutantscan -s and add filters for mutantscan, netscan, pslist

--- voltaire.py
+++ voltaire.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import os
+import re
 import sys
 from subprocess import call
 from sys import platform as _platform
@@ -11,16 +12,95 @@ is_windows = _platform == "win32"
 # Valid profiles
 valid_profiles = dict.fromkeys(
     ["VistaSP0x64", "VistaSP0x86", "VistaSP1x64", "VistaSP1x86", "VistaSP2x64", "VistaSP2x86", "Win10x64",
-     "Win10x64_10586", "Win10x64_14393", "Win10x86", "Win10x86_10586", "Win2003SP0x86", "Win2003SP1x64",
-     "Win10x86_14393", "Win2003SP1x86", "Win2003SP2x64", "Win2003SP2x86", "Win2008R2SP0x64", "Win2008R2SP1x64",
-     "Win2008R2SP1x64_23418", "Win2008SP1x64", "Win2008SP1x86", "Win2008SP2x64", "Win2008SP2x86", "Win2012R2x64",
-     "Win2012R2x64_18340", "Win2012x64", "Win2016x64_14393", "Win7SP0x64", "Win7SP0x86", "Win7SP1x64",
-     "Win7SP1x64_23418", "Win7SP1x86", "Win7SP1x86_23418", "Win81U1x64", "Win81U1x86", "Win8SP0x64", "Win8SP0x86",
-     "Win8SP1x64", "Win8SP1x64_18340", "Win8SP1x86", "WinXPSP1x64", "WinXPSP2x64", "WinXPSP2x86", "WinXPSP3x86"])
+     "Win10x86", "Win2003SP0x86", "Win2003SP1x64", "Win2003SP1x86", "Win2003SP2x64", "Win2003SP2x86",
+     "Win2008R2SP0x64", "Win2008R2SP1x64", "Win2008SP1x64", "Win2008SP1x86", "Win2008SP2x64",
+     "Win2008SP2x86", "Win2012R2x64", "Win2012x64", "Win7SP0x64", "Win7SP0x86", "Win7SP1x64", "Win7SP1x86",
+     "Win81U1x64", "Win81U1x86", "Win8SP0x64", "Win8SP0x86", "Win8SP1x64", "Win8SP1x86", "WinXPSP1x64",
+     "WinXPSP2x64", "WinXPSP2x86", "WinXPSP3x86"])
+
+
+def filter_mutantscan(args):
+    args["dest"] = os.path.abspath(args["dest"]) + os.sep
+    logfile = "{path}ES{number}_{command}.txt".format(path=args["dest"], number=args["es"],
+                                                      command="mutantscan -s")
+    outfile = "{path}mutantscan_filter.txt".format(path=args["dest"])
+    open_outfile = open(outfile, "w", encoding='utf-8')
+
+    nameMap = {}
+
+    with open(logfile, encoding='utf-8') as f:
+        add_line_flag = 0
+        open_outfile.write(next(f))
+        open_outfile.write(next(f))
+        for line in f:
+            for word in line.split():
+                if (not re.search("0x\w{16}|\.\.\.", word) and not word.isdigit()):
+                    add_line_flag = 1
+
+
+            if (add_line_flag == 1):
+                nameMap[word] = line
+                add_line_flag = 0
+
+        for key in sorted(nameMap, key=lambda v: v.upper()):
+            open_outfile.write(nameMap[key])
+
+
+def filter_netscan(args):
+    args["dest"] = os.path.abspath(args["dest"]) + os.sep
+    logfile = "{path}ES{number}_{command}.txt".format(path=args["dest"], number=args["es"],
+                                                      command="netscan")
+
+    outfile = "{path}netscan_filter.txt".format(path=args["dest"])
+    open_outfile = open(outfile, "w", encoding='utf-8')
+
+    with open(logfile, encoding='utf-8') as f:
+        add_line_flag = 0
+        open_outfile.write(next(f))
+        for line in f:
+            for word in line.split():
+                if(re.search("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", word)):
+                    add_line_flag = 1
+                    #if(word == )
+
+            if(add_line_flag == 1):
+                open_outfile.write(line)
+                add_line_flag = 0
+
+
+def filter_pslist(args):
+    path = os.path.abspath(args["dest"]) + os.sep
+    logfile = "{path}ES{number}_{command}.txt".format(path=path, number=args["es"], command="pslist")
+
+    with open(logfile, encoding='utf-8') as f:
+        for line in f:
+            command = "{command} -p {pid}".format(command="procmemdump", pid=line[3])
+            args["dest"] = "{path}{dump}ES{number}_{pid}.txt".format(path=path, dump="procmemdump" + os.sep,
+                                                                number=args["es"], pid=line[3])
+            individual_scan(args, command)
+
+
+def individual_scan(args, command):
+    is_valid(args)
+
+    program = os.path.abspath("vol.exe") if is_windows else "vol.py"
+
+    run_command(args, program, command)
 
 
 def scan(args):
-    args["src"] = os.path.abspath(args["src"])
+    is_valid(args)
+
+    program = os.path.abspath("vol.exe") if is_windows else "vol.py"
+
+    for command in ["pslist", "pstree", "netscan", "psxview", "consoles", "psscan", "mutantscan -s", "cmdscan",
+                    "dlllist", "filescan", "iehistory", "svcscan", "modules", "modscan", "sessions", "messagehooks",
+                    "windows", "wintree", "clipboard", "deskscan"]:
+        run_command(args, program, command)
+
+
+def is_valid(args):
+    args["src"] = "\"{path}\"".format(path=os.path.abspath(args["src"]))
     args["dest"] = os.path.abspath(args["dest"])
 
     if "src" in args:
@@ -43,38 +123,37 @@ def scan(args):
     else:
         print("NOTICE: No ES set. Defaulting to ES=1.")
 
-    program = os.path.abspath("vol.exe") if is_windows else "vol.py"
 
-    for command in ["pslist", "pstree", "netscan", "psxview", "consoles", "psscan", "mutantscan -s", "cmdscan",
-                    "dlllist", "filescan", "iehistory", "svcscan", "modules", "modscan", "sessions", "messagehooks",
-                    "windows", "wintree", "clipboard", "deskscan"]:
-        print("Starting {command}".format(command=command))
+def run_command(args, program, command):
+    path = args["dest"] + os.sep
+    outfile = "{path}ES{number}_{command}.txt".format(path=path, number=args["es"], command=command)
 
-        path = args["dest"] + os.sep
-        outfile = "{path}ES{number}_{command}.txt".format(path=path, number=args["es"], command=command)
-
-        if "profile" in args:
-            params = "-f {src} --profile={profile} {command} --output-file={dest}".format(src=args["src"],
+    if "profile" in args:
+        params = "-f {src} --profile={profile} {command} --output-file=\"{dest}\"".format(src=args["src"],
                                                                                           profile=args["profile"],
                                                                                           command=command,
                                                                                           dest=outfile)
-        else:
-            params = "-f {src} {command} --output-file={dest}".format(src=args["src"], command=command, dest=outfile)
+    else:
+        params = "-f {src} {command} --output-file=\"{dest}\"".format(src=args["src"], command=command,
+                                                                      dest=outfile)
 
-        result = call("{program} {params}".format(program=program, params=params))
+    print("{program} {params}".format(program=program, params=params))
+    result = call("{program} {params}".format(program=program, params=params))
 
-        if result == 0:
-            print("Completed {command}".format(command=command))
-        else:
-            print("Error running {command}".format(command=command))
+    if result == 0:
+        print("Completed {command}".format(command=command))
+    else:
+        print("Error running {command}".format(command=command))
 
     if is_windows:
         path = args["dest"] + os.sep
-        outfile = "{path}ES{number}_autorun.txt".format(path=path, number=args["es"])
+        outfile = "\"{outfile}\"".format(outfile="{path}ES{number}_autorun.txt".format(path=path, number=args["es"]))
+        print("Starting {command}".format(command=command))
 
         if "profile" in args:
             params = "-f {src} --profile={profile} printkey \"Software\\Microsoft\\Windows\\CurrentVersion\\Run\" --output-file={dest}".format(
                 src=args["src"], profile=args["profile"], dest=outfile)
+
         else:
             params = "-f {src} printkey \"Software\\Microsoft\\Windows\\CurrentVersion\\Run\" --output-file={dest}".format(
                 src=args["src"], dest=outfile)
@@ -84,7 +163,7 @@ def scan(args):
         if result == 0:
             print("Completed autorun")
         else:
-            print("Error running autorun")
+            print("Error running autorun \n " + params)
 
     print("Volatility files saved to {dest}".format(dest=args["dest"]))
 
@@ -118,6 +197,9 @@ if __name__ == "__main__":
 
     if "scan" == subcommand:
         scan(args)
+        filter_mutantscan(args)
+        filter_netscan(args)
+        filter_pslist(args)
     elif "process" == subcommand:
         process(args)
     else:
